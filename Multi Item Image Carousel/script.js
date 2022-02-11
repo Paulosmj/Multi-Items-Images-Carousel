20443d96fb6c4321a844e936a31871159d3ec2bb
Paulo S Meneguesso <83673028+Paulosmj@users.noreply.github.com>
2022-02-11 17:14:31 -0300

    Add files via upload

---
+++ Multi Item Image Carousel/script.js
@@ -0,0 +1,26 @@
+let slider = tns({
+    container: ".my-slider",
+    "slideBy": "1",
+    "speed": 400,
+    "nav": false,
+    autoplay: true,
+    controls: false,
+    autoplayButtonOutput: false,
+    responsive: {
+        1600: {
+            items: 4,
+            gutter: 20
+        },
+        1024: {
+            items: 3,
+            gutter: 20
+        },
+        768: {
+            items: 2,
+            gutter: 20
+        },
+        480: {
+            items: 1
+        }
+    }
+})
